0b691cd5a2fd661a9847b36bd8e57d4e81e6e0ae
ajaydeta <ajaydeta02@gmail.com>
2021-02-19 11:08:27 +0700

    revisi tulisan sama modal

--- asset/js/script.js
+++ asset/js/script.js
@@ -2,3 +2,8 @@ function modalTriger(id) {
     var modal = document.getElementById(id);
     modal.style.visibility = 'visible';
 }
+
+function modalClose(id) {
+    var modal = document.getElementById(id);
+    modal.style.visibility = '';
+}
