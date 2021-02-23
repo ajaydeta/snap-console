55608d94830c57e6102d01804e9f2425eef6d222
ajaydeta <ajaydeta02@gmail.com>
2021-02-23 10:06:07 +0700

    wrapper animasi typografi

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,9 +1,13 @@
 function modalTriger(id) {
     var modal = document.getElementById(id);
+    var overlay = document.getElementById('overlay');
     modal.style.visibility = 'visible';
+    overlay.style.display = 'block';
 }
 
 function modalClose(id) {
     var modal = document.getElementById(id);
+    var overlay = document.getElementById('overlay');
     modal.style.visibility = '';
+    overlay.style.display = 'none';
 }
